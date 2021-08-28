7ba0b96d1d390d4f587bfc9304be1bc95c6bd965
a-kichi <arsnino17aki24@gmail.com>
2021-08-29 02:36:24 +0900

    add modal

--- popup.js
+++ popup.js
@@ -1,21 +1,42 @@
-function popupImage() {
-    var popup = document.getElementById('js-popup');
-    if(!popup) return;
+// function popupImage() {
+//     var popup = document.getElementById('js-popup');
+//     if(!popup) return;
   
-    var blackBg = document.getElementById('js-black-bg');
-    var closeBtn = document.getElementById('js-close-btn');
-    var showBtn = document.getElementById('js-show-popup');
+//     var blackBg = document.getElementById('js-black-bg');
+//     var closeBtn = document.getElementById('js-close-btn');
+//     var showBtn = document.getElementById('js-show-popup');
   
-    closePopUp(blackBg);
-    closePopUp(closeBtn);
-    closePopUp(showBtn);
-    function closePopUp(elem) {
-      if(!elem) return;
-      elem.addEventListener('click', function() {
-        popup.classList.toggle('is-show');
-      });
-    }
-  }
-  popupImage();
-
+//     closePopUp(blackBg);
+//     closePopUp(closeBtn);
+//     closePopUp(showBtn);
+//     function closePopUp(elem) {
+//       if(!elem) return;
+//       elem.addEventListener('click', function() {
+//         popup.classList.toggle('is-show');
+//       });
+//     }
+//   }
+//   popupImage();
   // https://tech-dig.jp/js-modal/
+
+  const modalWrapOpen = function(e) {
+    const dataModalOpen = e.currentTarget.dataset.modalOpen;
+    Array.from(document.querySelectorAll('.popup')).forEach((e, i) => {
+      if(e.getAttribute('data-modal') === dataModalOpen){
+        e.classList.toggle('is-show');
+      }
+    })
+  }
+  
+  Array.from(document.querySelectorAll('.default-content')).forEach((modalOpenElement) => {
+    modalOpenElement.addEventListener('click', modalWrapOpen);
+  })
+  
+  const modalCloseAction = function(e) {
+    const targetModal = e.currentTarget.closest('.popup');
+    targetModal.classList.toggle('is-show')
+  };
+  
+  Array.from(document.querySelectorAll('.close-btn, .black-background')).forEach((modalCloseElement) => {
+    modalCloseElement.addEventListener('click', modalCloseAction)
+  })

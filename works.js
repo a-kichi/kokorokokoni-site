ebe6db187ce0527bac32fdbf73abc8daf587b258
mizucoffee <develop@mizucoffee.net>
2021-09-16 19:28:41 +0900

    [update] modal

---
+++ works.js
@@ -0,0 +1,140 @@
+window.works = {
+  ciel: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  jesse: {
+    title: 'ジェシーの作品',
+    description: '作品の説明',
+    author: '飯田 梨花',
+    authorMemo: '作者の説明'
+  },
+  gakki: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  'a-kichi': {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  antony: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  takoyaki: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  kimukimu: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  miyuseru: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  kony: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  mizu: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  sawakkuma: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  chihiro: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  ayumin: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  ebisuke: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  lila: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  see: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  tomitan: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  ronron: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  switchro: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  gassi: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  mittan: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  watayo: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  },
+  co4: {
+    title: '',
+    description: '',
+    author: '',
+    authorMemo: ''
+  }
+}

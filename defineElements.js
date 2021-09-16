f92accdc7d6958e6de2107e147177bbadcf2204b
mizucoffee <develop@mizucoffee.net>
2021-09-16 19:56:12 +0900

    [add] webp

--- defineElements.js
+++ defineElements.js
@@ -3,9 +3,16 @@ class BeforeButton extends HTMLElement {
     super();
 
     const photoId = this.getAttribute('data-photo-id')
+    const picture = document.createElement('picture')
+    const source = document.createElement('source')
+    source.srcset = `/img/before-img/bfr-${photoId}.webp`
+    source.type = 'image/webp'
     const img = document.createElement('img')
     img.src = `/img/before-img/bfr-${photoId}.png`
-    this.appendChild(img)
+
+    picture.appendChild(source)
+    picture.appendChild(img)
+    this.appendChild(picture)
   }
 }
 

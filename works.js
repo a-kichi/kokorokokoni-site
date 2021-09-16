98ef573ba59236800f4bef6936daa3c84a27b84e
a-kichi <arsnino17aki24@gmail.com>
2021-09-16 20:41:56 +0900

    add modal description

--- works.js
+++ works.js
@@ -1,138 +1,138 @@
 window.works = {
   ciel: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「息を呑む」',
+    description: '六甲山のミーツアートを訪れた際の夕焼けです。芸術に集う人々がみんな、刻一刻と変化する空を思わず息をするのも忘れたかのように見つめていました。空が私たちを惹きつけ離さないのは、同じ空は二度と見られない儚さからなのかもしれません。',
+    author: '安念 玉希',
     authorMemo: ''
   },
   jesse: {
-    title: 'ジェシーの作品',
-    description: '作品の説明',
+    title: '「午後３時、集合場所」',
+    description: '夏になると、自分たちだけの名前をいろんな所につけて、流れる汗も気にせず、いつまでも遊んでいた小学生の夏休みを思い出します。自転車さえあればどこでもいける。そう思っていた当時の気持ちを、遠くにある、立ち入り禁止の赤い煙突に見立てました。この写真を通して、皆さんの夏の思い出を思い出してもらえると嬉しいです。　',
     author: '飯田 梨花',
     authorMemo: '作者の説明'
   },
   gakki: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「Photo by 映ルンです」',
+    description: '名古屋の港で写ルンですを用いて撮影した写真です。今まであまり写真を撮らない性格でしたが誘われて初めて写ルンですを使って撮影しました。この名古屋の海は自分にとって初めて上手に撮れた写真として気に入っています。',
+    author: '稲垣 佑真',
     authorMemo: ''
   },
   'a-kichi': {
-    title: '',
-    description: '',
-    author: '',
-    authorMemo: ''
+    title: '「記憶」',
+    description: 'がむしゃらに遊んでいた当時の記憶と、いつの間にか大人になってしまった自分。変わったこと、変わらないことを見つける時間の一枚です。',
+    author: '奥村 彰子',
+    authorMemo: '奈良の大学でまちづくりについて学んでいます。デザインが好きで、WEBサイトやフリーペーパーのデザインなどもしています。'
   },
   antony: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「二人の時間」',
+    description: '初めは死ぬほど嫌いだった。決まって、死んだ街にはオマエが落ちてた。出会ったのはハタチの夏。気付いたらいつも救われてた。オマエといるとみんな俺を避ける。でもオマエは悪くないって俺は知ってる。今日も心躍らせ、頬に触れる。',
+    author: '神原 良継',
     authorMemo: ''
   },
   takoyaki: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「命脈」',
+    description: '凛とした菊を愛でる。生きることが明とも言えず、また死ぬことも暗とは言えない。ただ、今日を懸命に生きようと思う。',
+    author: '木村 航也',
     authorMemo: ''
   },
   kimukimu: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「かさなり」',
+    description: 'きっと自分はいつまでも白黒で単純に割り切ることはできない。日常の何気のない会話が自分の中に積み重なっていく。周りの色を少しずつ取り入れて自分を作っている。そんな生活の中での曖昧な思いつきが自分にとって大切なんだと思う。',
+    author: '木村 俊也',
     authorMemo: ''
   },
   miyuseru: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「影と光」',
+    description: 'いろいろな奇跡が積み重なって我が家に来た猫の影丸（かげまる）です。なににでも興味を示して視界にはいったものを追っかけて、いつでもワクワクドタバタしている無邪気さに心を動かされています。影で見つかったグレーの猫だったので名前は影丸。でも、我が家にきたことで光を見つけてくれているといいな〜。',
+    author: '桑名 未悠',
     authorMemo: ''
   },
   kony: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「ひさしぶり」',
+    description: 'この時代だからこそ出会えて、この時代だからこそ会えない人。きっとそんな人との思い出こそ、何年経っても心の動くきっかけになるんだと思います',
+    author: '小西 星七',
     authorMemo: ''
   },
   mizu: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「霖雨の空を仰いで」',
+    description: '空を仰げば、路地に聳える無機質な電柱、暗闇の中に走る送電線。眩く光る街灯、照らされた雨滴弾ける夏の子夜。',
+    author: '五嶋 律樹',
     authorMemo: ''
   },
   sawakkuma: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「Kumahead」',
+    description: 'Please, could you stop the noise? I’m trying to get some rest.From all the unborn chicken voices In my head.',
+    author: '澤野 雄太',
     authorMemo: ''
   },
   chihiro: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「17時15分」',
+    description: '撮影 kodak M35. 窓の外からはメジロの鳴き声と。仕事から帰ったじいちゃんは寝ながら相撲を見て、部活もない私は出来立てのおかずをつまみ食いをした。いまもずっと変わらず立ち続けてくれている背中が大好きで、遠くにいても大事にして生きたい。',
+    author: '杉原 千尋',
     authorMemo: ''
   },
   ayumin: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「羨望」',
+    description: '暗闇のなか上へ進む光と立ち止まる自分。',
+    author: '武本 歩美',
     authorMemo: ''
   },
   ebisuke: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「ゆらめき」',
+    description: '静かに時の流れに寄りそう漁港の町で、水のゆらぎをあたたかく照らし出している船首に目を引き寄せられた。この光はこの町そのものを映していたのかもしれない。',
+    author: '田尻愛奈',
     authorMemo: ''
   },
   lila: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「Moi」',
+    description: 'Moiとはフランス語で「私」と言います。私のハンドルネームはリラと言います。リラはライラックの花をフランス語でそう呼ぶのです。私は今まで感じる情景写真などは後からの意味付けに過ぎず。その写真はただ撮って「エモいと感じたから」という気持ちでしか撮っていませんでした。私はその時、衝動的に私という被写体が存在しているということを多くの人に知って貰いたいそう感じたのです。この一枚で私という人物を知ることができるかと言われるとそうではないでしょう。ですが、この一枚であなた自身が感じる私という想像を膨らましてほしいと感じるのです。あなたから見た私はどのように写って見えるのでしょう。あなたの感じる私を沢山想像し情動してください。',
+    author: '辰巳真由',
     authorMemo: ''
   },
   see: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「歩き方」',
+    description: '砂で歩きにくくても、ここまできた。これからも進んでく。という想いを込めてみました。たくさんの足跡でわからなくなったここまで歩いてきた道のり、どこにでも行けるこの先の道たくさんのものを想像してみてください。',
+    author: '谷井 聖奈',
     authorMemo: ''
   },
   tomitan: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「京の夜」',
+    description: '美しい京都の夜の街並みを撮影しました。京都の街並みの美しさと歴史を表現するために、まるでフィルムカメラで撮ったかのようにレタッチしました。普段見慣れてしまった街の雰囲気も、視線を変えて見ると趣を何度でも感じることができますね。',
+    author: '冨田 悠斗',
     authorMemo: ''
   },
   ronron: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「あの刻の貴方と」',
+    description: '「すれ違うことのない貴方。思ひ出となった貴方。でも、確かに貴方がいたこの地。色褪せようとも、雑色に紛れようとも貴方を感じる。」違う刻を過ごした人を想う一枚。',
+    author: '長野 彩',
     authorMemo: ''
   },
   switchro: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「ガラスに映る」',
+    description: 'グランフロント大阪に映る夕焼けを写真に収めました。うめきたが開発されるにつれ、いずれは新しく作られる建造物に遮られてしまう景色を残しておきたいと思いました。ガラスに反射する夕焼けの美しさとその中で働く人の美しさという共通点を映し出すことを意識しました。',
+    author: '西村 颯一郎',
     authorMemo: ''
   },
   gassi: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「流動」',
+    description: '世の中の一切のものは、生まれては消滅することを繰り返す。この世に存在する限りは、常に変化し動くことを止めない。ほんの少しも留まることはない。だから、この世の中は面白いし常に進化をとげることができる。',
+    author: '東田 陽',
     authorMemo: ''
   },
   mittan: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「小雨」',
+    description: '深夜の都会。静かに降る雨と満員の列車。その後ろにある暖かい家庭。スマホで撮影した写真をレタッチしています！雨で反射する踏切のライトと速度感のある電車の雰囲気感がイチオシポイントです！',
+    author: '水田 将大',
     authorMemo: ''
   },
   watayo: {
-    title: '',
-    description: '',
-    author: '',
+    title: '「焚き火」',
+    description: '情動とは、一時的で急激な感情の動きを意味すると言われています。これは、突発的な出来事によって起こるだろうと想像できます。しかし自分は、むしろ落ち着いた精神状態にあるときに、様々な感情が引き起こされることが多いと考えました。何も考えずに焚き火をみると、非常に複雑な感情に支配されます。それを表現したくて、今回の写真を撮影しました。ぼーっと見ていられるような焚き火の雰囲気を意識しました。',
+    author: '渡辺 亮',
     authorMemo: ''
   },
   co4: {
-    title: '',
+    title: '「こころ、ここに。」',
     description: '',
     author: '',
     authorMemo: ''
